d0ed55c1fa86e4e00d95a897fc2f66cc22e29155
Joy879 <joywanjiru879@gmail.com>
2023-04-28 17:11:00 +0300

    Modify app logic and add data subsets

--- app.R
+++ app.R
@@ -1,24 +1,20 @@
-##-----------------Library imports---------------------------------------------------##
 library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(plotly)
 library(glue)
-## Also include this else dashboard deployment fails
 library(maps)
-
-
-##-------------------Obtain the world map --------------------------------------------##
+# #Obtain the world map --------------------------------------------
 world1 <- map_data("world")
 
-##-------------------------Get Covid Data----------------------------------------------##
+## Get Covid Data------------------------------------
 covid <- read.csv("data/WHO-COVID-19-global-data.csv")
 
 covid$Month <- format(as.Date(covid$Date_reported), "%m")
 covid$Year <- format(as.Date(covid$Date_reported), "%Y")
 covid$Day <- format(as.Date(covid$Date_reported), "%d")
 covid$weekday <- wday(covid$Date_reported, label=TRUE, abbr=FALSE)
-##-------------------------------Create selection values --------------------------------##
+## Create selection values --------------
 cases <- select(covid, New_cases:Cumulative_deaths)
 
 
@@ -31,6 +27,7 @@ covid_max_date <- max(covid$Date_reported)
 ### Change font sizes of titles and ticks. Also look into themes
 ### Figure out how to get data directly from website
 ### Bring logic for different colors if cases/deaths/vaccinations
+### Fix breaking deployment issues
 ### Add vaccination data and add map visualization and color logic for that as well
 ### Look into proper mapping using leaflet/folium/choropleth
 
@@ -38,25 +35,21 @@ covid_max_date <- max(covid$Date_reported)
 ### Add logic to update the regions in descending order and rename regions to official names.
 ### Filter data to focus on vaccination ratio/infection ratio
 
-##-----------------------------UI Section-------------------------------------------------------------------------## 
+
 ui <- dashboardPage(
-  # Header
+  
   dashboardHeader(title="Covid Dashboard"),
-  # Sidebar
   dashboardSidebar(
     sidebarMenu(
       menuItem("World analysis",
                tabName="map"),
       menuItem("Per region analysis",
                tabName="region")
-      ## TODO add vaccination section either as a section or as part of the world and region analysis
     )
   ),
-  ## App content
   dashboardBody(
     tabItems(
       tabItem(tabName = "map",
-              ### Probably reduce size or use other widgets
               fluidRow(
                 box(selectInput("case_death",
                                 "Case/Death distribution",
@@ -67,7 +60,7 @@ ui <- dashboardPage(
               br(),
               br(),
               plotlyOutput("reg1")),
-     # Region analysis. TODO: Add logic to sort graphs by region with most cases/deaths and show names and totals for each region 
+      
     tabItem(tabName = "region",
             selectInput("case_death1",
                         "Case/Death distribution",
@@ -119,20 +112,24 @@ server <- function(input,output) {
     mapdata <- left_join(world1, data2, by="region")
     p<-mapdata%>% ggplot(aes(x = long,
                              y = lat, 
-                             text=paste("Country: ", region, "<br>",input$case_death, get(input$case_death), "<br>"))) + 
-    geom_polygon(aes(group=group,fill=get(input$case_death)))+ 
-    labs(title = glue('World map of {input$case_death} as at {input$minimum_date}'), fill=input$case_death) + 
-    coord_fixed(ratio=1)+ 
-    theme( axis.ticks = element_blank(),# remove the vertical grid lines
-           panel.grid.major.x = element_blank(),
-           panel.grid.major.y = element_blank(),
-           panel.background = element_blank(),
-           axis.text.x=element_blank(), #remove x axis labels
-           axis.ticks.x=element_blank(), #remove x axis ticks
-           axis.text.y=element_blank(),  #remove y axis labels
-           axis.ticks.y=element_blank(),  #
-           plot.title = element_text(hjust = 0.5))+ 
-    theme(text = element_text(family = "Times New Roman"))
+                             text=paste( "Country: ", region, "<br>",
+                                         input$case_death, get(input$case_death), "<br>"))) +
+      geom_polygon(aes(group=group,
+                       fill=get(input$case_death)))+
+      labs(title = glue('World map of {input$case_death} as at {input$minimum_date}'),
+           fill=input$case_death) +
+      coord_fixed(ratio=5)+ 
+      theme( axis.ticks = element_blank(),
+             # remove the vertical grid lines
+             panel.grid.major.x = element_blank(),
+             panel.grid.major.y = element_blank(),
+             panel.background = element_blank(),
+             axis.text.x=element_blank(), #remove x axis labels
+             axis.ticks.x=element_blank(), #remove x axis ticks
+             axis.text.y=element_blank(),  #remove y axis labels
+             axis.ticks.y=element_blank(),  #
+             plot.title = element_text(hjust = 0.5))+
+      theme(text = element_text(family = "Times New Roman"))
     
     ggplotly(p, tooltip=c("text"))
   }) 
@@ -145,32 +142,34 @@ server <- function(input,output) {
     g<-res%>%arrange(desc(total))%>%ggplot(aes(y=total,
                                                x=WHO_region,
                                                fill=WHO_region,
-                                               text=paste("Date Reported: ", input$minimum_date, "<br>",input$case_death, total, "<br>")))+
-    geom_col()+ 
-    labs(title = glue('WHO Region total {input$case_death} as at {input$minimum_date}'),
-         y=input$case_death)+coord_flip()+
-    xlab("") + 
-    ylab("") +
-    theme(plot.title = element_text(hjust = 0.5),
-          panel.grid.major.x = element_blank(),
-          panel.grid.major.y = element_blank(),
-          axis.text.x=element_blank(), #remove x axis labels
-          axis.ticks.x=element_blank(), #remove x axis ticks
-          axis.text.y=element_blank(),  #remove y axis labels
-          axis.ticks.y=element_blank(),
-          panel.background = element_blank())+
-    theme(text = element_text(family = "Times New Roman"))
+                                               text=paste("Date Reported: ", input$minimum_date, "<br>",
+                                                          input$case_death, total, "<br>" )))+
+      geom_col(position='dodge')+ 
+      labs(title = glue('WHO Region total {input$case_death} as at {input$minimum_date}'),
+           y=input$case_death)+
+      coord_flip()+
+      xlab("") + 
+      ylab("") +
+      theme(plot.title = element_text(hjust = 0.5),
+            panel.grid.major.x = element_blank(),
+            panel.grid.major.y = element_blank(),
+            axis.text.x=element_blank(), #remove x axis labels
+            axis.ticks.x=element_blank(), #remove x axis ticks
+            axis.text.y=element_blank(),  #remove y axis labels
+            axis.ticks.y=element_blank(),
+            panel.background = element_blank())+
+      theme(text = element_text(family = "Times New Roman"))
     ggplotly(g, tootltip=c("text"))
   })
-  
-  ## Function to create per region bar graphs
   regiongraph <- function(region, years, cases, color){
-    graph<-covid%>%subset(Year == years)%>%filter(WHO_region == region)%>%ggplot(aes(y=get(cases), x=Date_reported,text=paste(
-      cases, get(cases), "<br>"
-    )))+geom_col(fill=color)+labs(y=cases)+
-      theme( # remove the vertical grid
-        panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank() )
+    data <- read_csv(glue('data/covid{years}.csv'))
+    graph<-data%>%filter(WHO_region == region)%>%ggplot(aes(y=get(cases),
+                                                            x=Date_reported,
+                                                            text=paste(cases, get(cases), "<br>")))+
+      geom_col(fill=color)+
+      labs(y=cases)+
+      theme(panel.grid.major.x = element_blank(),
+            panel.grid.major.y = element_blank() )
     ggplotly(graph, tootltip=c("text"))
     
   }
